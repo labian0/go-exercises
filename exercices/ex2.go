f369f228695ca333a55427b8ce4b83f2afcf50bc
rodolphe <rodolphe.s@proton.me>
2024-12-03 00:01:34 +0100

    exo1 + 2

--- exercices/ex2.go
+++ exercices/ex2.go
@@ -43,23 +43,49 @@ func post(jsonData []byte, url string) (ResponseData, error) {
 	//renvoyer la donnée
 }
 
-func genererNombre() int {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	randomNumber := r.Intn(100)
-	return randomNumber
+// checker si un nombre est premier
+func estPremier(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
-func ex1() {
-	// JEU DEVIN
-	choix := genererNombre()
+// generer un nombre premier de n chiffres
+func genererPremier(n int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	min := intPow(10, n-1)
+	max := intPow(10, n) - 1
 
-	json := []byte(fmt.Sprintf(`{"key":"%d"}`, choix))
-
-	response, err := post(json, "https://url/")
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
+	for {
+		num := r.Intn(max-min+1) + min // Generate a number in the range [min, max]
+		if estPremier(num) {
+			return num
+		}
 	}
+}
 
-	//TODO: traiter la réponse et trouver le nombre
+// calculer la puissance d'un nombre
+func intPow(base, exp int) int {
+	result := 1
+	for exp > 0 {
+		result *= base
+		exp--
+	}
+	return result
+}
+
+func main() {
+	choix := genererPremier(19)
+	fmt.Println(choix)
+	/*
+	   json := []byte(fmt.Sprintf(`{"key":"%d"}`, choix))
+
+	   response := post(json, "https://url/")
+	*/
 }
